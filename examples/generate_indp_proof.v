03b99486c544ac492e0f83f70ce696c8610d8fab
WeituoDAI <weituo.dai.2020@polytechnique.org>
2024-07-19 17:21:13 +0200

    support letin, non uniform param, lambda type arg

--- examples/generate_indp_proof.v
+++ examples/generate_indp_proof.v
@@ -1,14 +1,10 @@
 Require Import MetaCoq.Programming.
-Require Import generate_inductive_principle.
-
-(* From MetaCoq Require Export bytestring. *)
 Global Open Scope bs.
 
 Definition the_name := {| binder_name := nNamed "x"; binder_relevance := Relevant |}.
 Definition prop_name := {| binder_name := nNamed "P"; binder_relevance := Relevant |}.
 Definition fix_name :=  {| binder_name := nNamed "F"; binder_relevance := Relevant |}.
 
-(* Print redo. *)
 
 Fixpoint n_tl {A} (l:list A) n :=
   match n with
@@ -17,25 +13,18 @@ Fixpoint n_tl {A} (l:list A) n :=
   end
 .
 
+Axiom print_context: forall {A}, infolocal -> A.
+Axiom print_term : forall {A}, term -> A.
+Axiom print_renaming: forall {A}, list (BasicAst.context_decl term) -> A.
 
-Axiom print_context: forall {A}, context -> A.
-
-
-(* Search (nat -> string). *)
-(* Print tFix. *)
-
-(* Print rev_app. *)
-
+(* Print fold_left_i. *)
 
 Definition GenerateIndp_proof (kername : kername) (ty :  mutual_inductive_body) : term :=
 
   let params := ty.(ind_params) in
   let n_ind := length ty.(ind_bodies) in
-  let initial_info : infolocal :=
-    let e := make_initial_info kername ty in
-    e
-  in
-
+  let initial_info := make_initial_info kername ty in
+  let (params, no_uniform_params) := SeperateParams kername ty in
 
   let aux (e:infolocal) (b:list constructor_body) (j:nat) (t:infolocal -> term) :term :=
     let auxctr (i:nat) (ctr:constructor_body) (e:infolocal): term :=
@@ -44,15 +33,25 @@ Definition GenerateIndp_proof (kername : kername) (ty :  mutual_inductive_body) 
       let transformer_result :infolocal -> term := fun e =>
         tApp (geti_info "P" e j)
           (
+            rels_of "no_uniform_params" e
+            ++
             (map (mapt e) ctr.(cstr_indices))
             ++
             [tApp constructor_current
-              (rels_of "params" e ++ rels_of "args" e)]
+              (rels_of "params" e ++ rels_of "no_uniform_params" e ++ rels_of "args" e)]
           )
       in
       let auxarg arg (t:infolocal->term) :infolocal -> term := fun e =>
         let t1 := arg.(decl_type) in
         let na := arg.(decl_name) in
+
+        match arg.(decl_body) with
+        | Some t0 => kptLetIn NoSave e t0 na (mapt e t0) (mapt e t1) t
+        | None =>
+          match normal e t1 with
+          | None => print_context e
+          | Some t1 =>
+
         match t1 with
         | tRel i =>
           match is_rec_call e i with
@@ -66,7 +65,7 @@ Definition GenerateIndp_proof (kername : kername) (ty :  mutual_inductive_body) 
               (fun e =>
                 kptProd NoSave e the_name
                   (
-                    tApp (geti_info "P" e kk) (*tRel 0*)[get_info_last "args" e])
+                    tApp (geti_info "P" e kk) [get_info_last "args" e])
                   t)
           | None =>
             kptProd (Savelist "args") e na
@@ -130,48 +129,85 @@ Definition GenerateIndp_proof (kername : kername) (ty :  mutual_inductive_body) 
                 (mapt e t1)
                 t
         end
+        end end
       in
       fold_left_ie (fun _ => auxarg) ctr.(cstr_args) (transformer_result) e
     in
     fold_right_ie
       (fun i a t e =>
         mktLambda (Savelist (String.append "Pctr" (string_of_nat j) )) e the_name
-          (auxctr i a e) t
+          (it_kptProd_default (Some "no_uniform_params") e (no_uniform_params) $
+            auxctr i a) t
       ) b t e
   in
 
   let aux' : infolocal -> Nat.t -> Nat.t -> constructor_body -> (context * (infolocal -> term)) :=
     fun e j i b =>
-    (b.(cstr_args), fun e =>
-      tApp (geti_info (String.append "Pctr" (string_of_nat j)) e i)
-        (fold_left
-          (fun a b => app b a)
-              (let auxarg arg e :=
-                let arg_current := get_arg_current e in
-                match arg.(decl_type) with
-                | tApp (tRel j) tl =>
-                  match is_rec_call e j with
-                  | None => [arg_current]
-                  | Some kk =>
-                    [arg_current;
-                    tApp
-                      (geti_info "F" e kk)
-                        ((map (mapt e) (n_tl tl (length params)) )
-                          ++ [arg_current])]
-                  end
-                | tRel j =>
-                  match is_rec_call e j with
-                  | None => [arg_current]
-                  | Some kk =>
-                    [ arg_current;
-                      tApp (geti_info "F" e kk) [arg_current]]
-                  end
-                | tProd _ _ _ => todo
-                | _ => [arg_current] end
-              in
-              map_with_infolocal_arg auxarg b.(cstr_args) e)
-              []
-        )
+    (b.(cstr_args),
+     fun e =>
+      tApp (geti_info (String.append "Pctr" (string_of_nat j)) e i) $
+        List.app (rels_of "no_uniform_params" e) $
+        List.concat
+          (let auxarg arg e :=
+            let arg_current := get_arg_current e in
+            match arg.(decl_body) with
+            | Some _ => []
+            | None =>
+            let t1 := arg.(decl_type) in
+            match normal e t1 with
+            | None => print_context e
+            | Some t1 =>
+            match t1 with
+            | tApp (tRel j) tl =>
+              match is_rec_call e j with
+              | None => [arg_current]
+              | Some kk =>
+                [arg_current;
+                tApp
+                  (geti_info "F" e kk)
+                    ((map (mapt e) (n_tl tl (length params)) )
+                      ++ [arg_current])]
+              end
+            | tRel j =>
+              match is_rec_call e j with
+              | None => [ arg_current ]
+              | Some kk =>
+                [ arg_current;
+                  tApp (geti_info "F" e kk) [arg_current]]
+              end
+            (*****************************************)
+            | tProd na _ _ =>
+              match check_return_type t1 e with
+              | None => [ get_arg_current e ]
+              | Some _ =>
+                let fix aux_nested e t1 :=
+                  match t1 with
+                  | tProd na ta tb =>
+                    kptLambda (Savelist "arglambda") e na
+                      (mapt e ta) (fun e => aux_nested e tb)
+                  | tRel i =>
+                    match is_rec_call e i with
+                    | None => todo
+                    | Some kk =>
+                      tApp (geti_info "F" e kk)
+                        [tApp (get_arg_current e) (rels_of "arglambda" e)]
+                    end
+                  | tApp (tRel i) tl =>
+                    match is_rec_call e i with
+                    | None => todo
+                    | Some kk =>
+                      tApp (geti_info "F" e kk)
+                          (map (mapt e) (n_tl tl (length params)) ++
+                          [tApp (get_arg_current e) (rels_of "arglambda" e)])
+                    end
+                  | _ => todo
+                  end in
+                [ get_arg_current e; aux_nested e t1 ]
+              end
+            (*****************************************)
+            | _ => [arg_current] end end end
+          in
+          map_with_infolocal_arg auxarg b.(cstr_args) e)
     )
   in
   let bodies := ty.(ind_bodies) in
@@ -179,33 +215,52 @@ Definition GenerateIndp_proof (kername : kername) (ty :  mutual_inductive_body) 
   let generate_fix e j mainbody : def term :=
     let indices_main := mainbody.(ind_indices) in
     let the_inductive_main := {| inductive_mind := kername; inductive_ind := j|} in
-
+    let indice_is_tlet :=
+      map (fun t =>
+            match t.(decl_body) with
+            | Some _ => true
+            | None => false end
+          ) indices_main
+    in
     mktfixpoint (Savelist "F") (map (fun _ => fix_name) bodies) e
       fix_name
-      ( e <- it_mktProd_default (Some "indices") e indices_main;;
+      ( e <- it_kptProd_default (Some "no_uniform_params") e (no_uniform_params);;
+        e <- it_mktProd_default (Some "indices") e indices_main;;
         e <- mktProd (Saveitem "x") e the_name
               (tApp (tInd the_inductive_main [])
-                (rels_of "params" e ++ rels_of "indices" e));;
+                (rels_of "params" e ++ rels_of "no_uniform_params" e ++ rels_of "indices" e));;
         tApp (geti_info "P" e j)
-            (rels_of "indices" e ++ [rel_of "x" e])
-            )
+            (rels_of "no_uniform_params" e ++ rels_of "indices" e ++ [rel_of "x" e])
+      )
       (fun e =>
+        e <- it_kptLambda_default (Some "no_uniform_params") e (no_uniform_params);;
         e <- it_mktLambda_default (Some "indices") e (indices_main);;
         e <- mktLambda (Saveitem "x") e the_name
               (tApp (tInd the_inductive_main [])
-                (rels_of "params" e ++ rels_of "indices" e));;
+                (rels_of "params" e ++ rels_of "no_uniform_params" e ++ rels_of "indices" e));;
         (mktCase e
           {|  ci_ind := the_inductive_main;
-              ci_npar := length params;
+              ci_npar := length params + length no_uniform_params;
               ci_relevance := Relevant |}
           (fun _ => [])
-          (fun e => rels_of "params" e)
-          (fun e => repeat the_name (1 + length indices_main))
-          (fun e => tApp (geti_info "P" e j) (get_pcontext e))
+          (fun e => List.app (rels_of "params" e) (rels_of "no_uniform_params" e))
+
+          (fun e =>
+            repeat the_name (1 + length indices_main)
+          )
+
+          (fun e =>
+            tApp (geti_info "P" e j)
+              (rels_of "no_uniform_params" e
+                ++ get_pcontext_indices_without_tletin indice_is_tlet e
+                ++ [get_pcontext_var e]
+              )
+          )
+
           (fun e => rel_of "x" e)
           (fun e => mapi (aux' e j) mainbody.(ind_ctors)))
       )
-      (length indices_main)
+      (length indices_main + length no_uniform_params)
   in
 
   it_kptLambda_default (Some "params") initial_info params $
@@ -214,11 +269,11 @@ Definition GenerateIndp_proof (kername : kername) (ty :  mutual_inductive_body) 
       let the_inductive := {| inductive_mind := kername; inductive_ind :=i |} in
       let indices := body.(ind_indices) in
       mktLambda (Savelist "P") e prop_name
-        (
+       (e <- it_kptProd_default (Some "no_uniform_params") e (no_uniform_params);;
         e <- it_mktProd_default (Some "indices") e (indices);;
         tProd the_name
           (tApp (tInd the_inductive [])
-            (rels_of "params" e ++ rels_of "indices" e))
+            (rels_of "params" e ++ rels_of "no_uniform_params" e ++ rels_of "indices" e))
           (tSort sProp)
         ) t
   ) bodies
--- examples/test_indp_proof.v
+++ examples/test_indp_proof.v
@@ -51,3 +51,92 @@ with Config : Type :=
   .
 MetaCoq Run Derive InductivePrinciple Point as "indp_point".
 Print indp_point.
+
+
+Inductive le: nat -> nat -> Type :=
+| le_refl n : le n n
+| le_S n m : le (S n) m -> le n m.
+MetaCoq Run Derive InductivePrinciple le as "indp_le".
+Print indp_le.
+
+Inductive le' (n: nat): nat -> Type :=
+| le_refl' : le' n n
+| le_S' m : le' (S n) m -> le' n m.
+MetaCoq Run Derive InductivePrinciple le' as "indp_le'".
+Print indp_le'.
+
+
+Inductive Acc (A : Type) (R : A -> A -> Prop) (x : A) : Type :=
+	Acc_intro : (forall y : A, R y x -> Acc A R y) -> Acc A R x.
+MetaCoq Run Derive InductivePrinciple Acc as "indp_acc".
+Print indp_acc.
+
+
+
+
+Inductive myt0 (A:Type) (B: Type) : Type :=
+  | Build0 (x:A) (b:B) (c:
+    (fun x => match x with
+    | 0 => A
+    | _ => A end) (3)
+  ).
+MetaCoq Run Derive InductivePrinciple myt0 as "indp_myt0".
+Print indp_myt0.
+
+
+Inductive myt (A:Type) (B: Type) : Type :=
+  | Build (x:A) (b:B) (c:
+    (let fix f x :=
+      match x with
+      | 0 => A
+      | S n' => f n' end in f 3
+    )
+  ).
+MetaCoq Run Derive InductivePrinciple myt as "indp_myt".
+Print indp_myt.
+
+
+Inductive myt1 (A:Type) (B: Type) : Type :=
+  | Build1 (x:A) (b:B) (c:let x:= A in prod x B).
+MetaCoq Run Derive InductivePrinciple myt1 as "indp_myt1".
+Print indp_myt1.
+
+
+(* Inductive testletin : let x := nat in x -> Type :=
+  | C0: (let x := nat in x) -> (let y := option nat in prod y y ->
+          nat) -> let z := nat in let w := z in w -> testletin 0. *)
+(* MetaCoq Run PrintInductivePrinciple testletin. *)
+(* Unset Guard Checking. *)
+(* MetaCoq Run Derive InductivePrinciple testletin as "indp_testletin". *)
+(* Print testletin_ind. *)
+(* Load MetaCoqPrelude. *)
+(* Compute $Quote fun (P : let x := nat in forall x0 : x, testletin x0 -> Prop)
+(f : forall (p : let x := nat in x)
+     (n : let y := option nat in y * y -> nat),
+     let z := nat in let w := z in forall w0 : w, P 0 (C0 p n w0)) =>
+let x := nat in
+fun (f0 : x) (t : testletin f0) =>
+match t as t0 in (@testletin x0 f1) return (P f1 t0) with
+| C0 p n z w w0 => f p n w0
+end. *)
+
+
+Inductive mynat'' :Type :=
+  | myz''
+  | mys'' : let x := mynat'' in mynat'' -> mynat''.
+MetaCoq Run Derive InductivePrinciple mynat'' as "indp_mynat''".
+Print indp_mynat''.
+
+
+(* Inductive myvec' (A:Type) : let x := nat in x -> Type :=
+  | myvnil' : myvec' A 0
+  | myvcons' : let a := A in a -> forall n, let z := myvec' in z a n -> myvec' A (S n).
+MetaCoq Run Derive InductivePrinciple myvec' as "indp_myvec'".
+Print indp_myvec'. *)
+
+
+Inductive mynat' :Type :=
+  | myz'
+  | mys' : let x := mynat' in let y := x in ((fun z => z ) y) -> mynat'.
+MetaCoq Run Derive InductivePrinciple mynat' as "indp_mynat'".
+Print indp_mynat'.
